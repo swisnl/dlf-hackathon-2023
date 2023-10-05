b82ffae8d0f1eab78301b7a4c27bb9b6d25c3808
Anthony Schuijlenburg <aschuijlenburg@swis.nl>
2023-10-05 10:54:41 +0200

    feat: add tenants

---
+++ database/seeders/TenantSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tenant;
+use Illuminate\Database\Seeder;
+
+class TenantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Tenant::factory()->count(10)->create();
+    }
+}
