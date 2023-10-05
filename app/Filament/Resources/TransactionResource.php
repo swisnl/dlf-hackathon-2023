2cf2de12aec8b0f37c306fbafd4d68b52db0ac27
Anthony Schuijlenburg <aschuijlenburg@swis.nl>
2023-10-05 12:22:14 +0200

    feat: add transactions to filament

---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-euro';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('tenant_id')
+                    ->relationship('tenant', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('account_id')
+                    ->maxLength(36),
+                Forms\Components\TextInput::make('grams_of_co2')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('cents_charged')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('tenant.name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('account_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('grams_of_co2')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('cents_charged')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+        ];
+    }
+}
