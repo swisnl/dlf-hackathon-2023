80b5a77c07daf6e50f06793ba77b4d99dd82a158
Pjotr van der Horst <pvanderhorst@swis.nl>
2023-10-05 13:47:04 +0200

    [8] feat: add stats svg

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\StatsController;
+use App\Models\Tenant;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,13 @@ Route::post('/transactions/webhook', [PaymentController::class, 'webhook'])->nam
 
 Route::get('/stats/account/{account}', [StatsController::class, 'showAccount']);
 Route::get('/stats/tenant/{tenant}', [StatsController::class, 'showTenant']);
+
+Route::get('/stats/{tenant}', static function (Tenant $tenant) {
+    return response(
+        view('stats.view', [
+            'tenant' => $tenant,
+            'centsCharged' => $tenant->transactions->pluck('cents_charged')->sum(),
+            'gramsOfCo2' => $tenant->transactions->pluck('grams_of_co2')->sum(),
+        ])->render()
+    )->header('Content-Type', 'image/svg+xml');
+});
