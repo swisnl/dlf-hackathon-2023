eaa693348b485402e2aa44c028fd52633f2ba7f7
Pjotr van der Horst <pvanderhorst@swis.nl>
2023-10-05 15:26:42 +0200

    feat: primary color: green

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Emerald,
             ])
             ->favicon(asset('favicons/favicon.svg'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
