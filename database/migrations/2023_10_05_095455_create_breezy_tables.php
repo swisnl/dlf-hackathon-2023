e32cdc6f94f42f2d899b9a6e3e53b0d386d5b322
Shift <shift@laravelshift.com>
2023-10-05 11:48:23 +0000

    Add PHP type hints

--- database/migrations/2023_10_05_095455_create_breezy_tables.php
+++ database/migrations/2023_10_05_095455_create_breezy_tables.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('breezy_sessions', function (
             Blueprint $table
@@ -30,7 +30,7 @@ return new class extends Migration
 
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('breezy_sessions');
     }
